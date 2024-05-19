fdc36d101d7a222d9257a18246a9fe8de0b4ef90
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2024-05-19 10:26:29 +0200

    Added navigation elements

--- capstone/little-lemon/src/components/Navigation.js
+++ capstone/little-lemon/src/components/Navigation.js
@@ -1,7 +1,14 @@
 function Navigation() {
     return (
         <nav>
-            <p>Navigation</p>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/reservations">Reservations</a></li>
+                <li><a href="/order-online">Order online</a></li>
+                <li><a href="/login">Login</a></li>
+            </ul>
         </nav>
     );
 }
